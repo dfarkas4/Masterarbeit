51c1357426b652bb1d3add843ac3ad5e9c07c499
dfarkasd4 <david.farkas@finanzchef24.de>
2018-07-07 23:28:08 +0200

    test commit01

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Hapi = require('hapi'),
+    mongojs = require('mongojs'),
+    db = mongojs('mongodb://david:asd111@ds129541.mlab.com:29541/rs_food', ['test_collection']),
+    testCollection = db.collection('test_collection');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+
+        return 'Hello, world!';
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: (request, h) => {
+
+        //return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
+
+        testCollection.find(function (err, docs) {
+            return docs;
+        })
+    }
+});
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();

574f3022133dfa7af5a35741579b514f586b0b82
dfarkasd4 <david.farkas@finanzchef24.de>
2018-08-28 00:07:40 +0200

    Make study sequence concat two sequences at once and disregard current_db just switch starting location instead every time token changes.

--- App/getStudySequence.js
+++ App/getStudySequence.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const mongoClient = require('mongodb').MongoClient,
     _ = require('lodash');
 
-async function getStudySequence(userToken, collectionName) {
+async function getStudySequence(userToken) {
     let dbConnection = await mongoClient.connect(process.env.DB_STR),
         currSequence = {};
 
@@ -13,29 +13,16 @@ async function getStudySequence(userToken, collectionName) {
 
     latinSquare = latinSquare[0];
 
-    if (latinSquare.current_token === userToken && latinSquare.current_db === collectionName) {
-        currSequence.currentLocation = latinSquare.current_db;
-        currSequence.currentSequence = latinSquare[latinSquare.current_sequence + ''];
+    if (latinSquare.current_token === userToken) {
+        currSequence.currentSequence = latinSquare[latinSquare.current_sequence + ''] + latinSquare[((latinSquare.current_sequence + 1) % 5) + ''];
         currSequence.currentStartingLocation = latinSquare.current_starting_location;
-    } else if (latinSquare.current_token === userToken && latinSquare.current_db !== collectionName) {
-        currSequence.currentLocation = collectionName;
-        currSequence.currentSequence = latinSquare[((latinSquare.current_sequence + 1) % 5) + ''];
-        currSequence.currentStartingLocation = latinSquare.current_starting_location;
-        await dbConnection.db().collection('latin_square').update(
-            { "entry_id": "FFFAAA" },
-            { "$set": {
-                "current_sequence": ((latinSquare.current_sequence + 1) % 5), "current_db": collectionName
-            }}
-        );
     } else if (latinSquare.current_token !== userToken) {
         currSequence.currentStartingLocation = latinSquare.current_starting_location === 'BERLIN' ? 'WATERLOO' : 'BERLIN';
-        currSequence.currentLocation = currSequence.currentStartingLocation;
-        currSequence.currentSequence = latinSquare[((latinSquare.current_sequence + 1) % 5) + ''];
+        currSequence.currentSequence = latinSquare[((latinSquare.current_sequence + 2) % 5) + ''] + latinSquare[((latinSquare.current_sequence + 3) % 5) + ''];
         await dbConnection.db().collection('latin_square').update(
             { "entry_id": "FFFAAA" },
             { "$set": {
-                    "current_sequence": ((latinSquare.current_sequence + 1) % 5),
-                    "current_db": currSequence.currentLocation,
+                    "current_sequence": ((latinSquare.current_sequence + 2) % 5),
                     "current_token": userToken,
                     "current_starting_location": currSequence.currentStartingLocation
             }}
